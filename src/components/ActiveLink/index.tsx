fead6f4c7cdf600c7c477cc437b664b1d85aa6dd
Thiago Bueno Costa <th.bueno.95@gmail.com>
2021-10-18 09:40:52 -0300

    upload ignews

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,26 @@
+import { ReactElement, cloneElement } from 'react';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+
+
+interface ActiveLinkProps extends LinkProps {
+    children: ReactElement;
+    activeClassName: string;
+}
+
+export function ActiveLink({ children, activeClassName, ...props }: ActiveLinkProps) {
+    const { asPath } = useRouter();
+
+    const className = asPath === props.href
+        ? activeClassName
+        : '';
+    
+
+    return (
+        <Link { ...props }>
+            { cloneElement( children, {
+                className,
+            }) }
+        </Link>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,64 @@
+import { GetStaticProps } from 'next';
+
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton/inde';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amout: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={ styles.contentContainer }>
+        <section className={ styles.hero }>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            New about<br />
+            the <span>React</span> world
+          </h1>
+
+          <p>
+            Get acess to all the publications<br />
+            <span>for { product.amout } month</span>
+          </p>
+
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JZfX8E0EvZ8OWoP3r980wfO', { 
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amout: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 HOURS
+  }
+}
